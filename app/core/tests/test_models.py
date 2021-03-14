02535e1b93980f31acb0a7868aa6223dea68e9d2
RogerITData <datapawelzajac@example.com>
2021-03-14 13:57:53 +0100

    Add whitespecies.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -18,7 +18,7 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
         """Test the email for new user is normalized"""
-        email ='test@LONDONAPPDEV.COM'
+        email = 'test@LONDONAPPDEV.COM'
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
@@ -31,7 +31,8 @@ class ModelTests(TestCase):
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
-        'test@londondeveloper.com', 'test123'
+        'test@londondeveloper.com'
+        , 'test123'
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
